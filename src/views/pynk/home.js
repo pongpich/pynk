7780fd4606925fe9b471f56715b50d091af3c7f3
Golf <g_peerapat@planforfit.com>
2023-07-25 12:02:09 +0700

    test

--- src/views/pynk/home.js
+++ src/views/pynk/home.js
@@ -107,12 +107,12 @@ class Home extends Component {
                 </div>
                 <div className="bebe_bubble" /*  style={{ backgroundImage: `url(${home2})`, backgroundSize: '500px 500px', }}  */>
 
-                    <div className="background_Image" style={{ backgroundImage: `url(${home2})`, backgroundSize: '1295px 589px' }} >
+                    {/* <div className="background_Image" style={{ backgroundImage: `url(${home2})`, backgroundSize: '1295px 589px' }} >
 
                         <div className="box-text-bubble">
                             <p className="text-bubble">คอร์สสอนออกกำลังกายสุดปังจากวงการเบเบ้ ที่จะพาคุณมาอัปเกรดความฟิต ด้วยโปรแกรม 8 สัปดาห์ ที่เบเบ้ออกแบบพิเศษให้เหมาะกับคุณ พร้อมการันตีความสนุก และผลลัพธ์ของการเปลี่ยนแปลง มาแล้วมากกว่าสิบรุ่น!</p>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
 
 
