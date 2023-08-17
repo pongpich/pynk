dfa75c1ad62f6271a46a9ba00d3601e4a7190e59
thanet@planforfit.com <lollipopwar-love@hotmail.co.th>
2023-08-17 18:37:13 +0700

    css สรุปรายการสั่งซื้อ

--- src/views/pynk/admin/shop_order_summary.js
+++ src/views/pynk/admin/shop_order_summary.js
@@ -4,8 +4,13 @@ import Footer from "../footer";
 
 import picture01 from "../../../assets/img/pynk/shop/group-37546.png";
 import arrow_left_line from "../../../assets/img/pynk/shop/arrow-left-s-line.png";
-
 import delete_bin_line from "../../../assets/img/pynk/shop/delete-bin-line.png";
+import Logo_payment_1 from "../../../assets/img/pynk/shop/Logo-payment-1.png";
+import Logo_payment_2 from "../../../assets/img/pynk/shop/Logo-payment-2.png";
+import Logo_payment_3 from "../../../assets/img/pynk/shop/Logo-payment-3.png";
+import Logo_payment_4 from "../../../assets/img/pynk/shop/Logo-payment-4.png";
+import Logo_payment_5 from "../../../assets/img/pynk/shop/Logo-payment-5.png";
+
 const Shop_order_summary = () => {
   const [plusNumber, setPlusNumber] = useState(1);
 
@@ -21,7 +26,7 @@ const Shop_order_summary = () => {
     <>
       <div className="box-order-summary">
         <div className="background-order-summary row">
-          <div className="col-12 col-md-8 justify-content">
+          <div className="col-12 col-md-7 justify-content">
             <div>
               <div className="order-summary">
                 <div className="order">
@@ -31,7 +36,7 @@ const Shop_order_summary = () => {
                       <img src={picture01} className="image-product-order" />
                     </div>
                     <div className="col-6">
-                      <p className="fitto-shop">
+                      <p className="fitto-shop margin-left-768">
                         FITTO PLANT PROTEIN “ MILK TEA FLAVOUR ”
                       </p>
                       <div className="plus-minus-model back-g  col-6">
@@ -69,7 +74,7 @@ const Shop_order_summary = () => {
                       <img src={picture01} className="image-product-order" />
                     </div>
                     <div className="col-6">
-                      <p className="fitto-shop">
+                      <p className="fitto-shop margin-left-768">
                         FITTO PLANT PROTEIN “ MILK TEA FLAVOUR ”
                       </p>
                       <div className="plus-minus-model back-g  col-6">
@@ -109,9 +114,41 @@ const Shop_order_summary = () => {
               </button>
             </div>
           </div>
-          <div className="col-12 col-md-4 justify-content">asd</div>
-        </div>
+          <div className="col-12 col-md-5 justify-content-576">
+            <div>
+              <div className="box-proceed-payment">
+                <div className="input-code">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="ใส่รหัสส่วนลด"
+                  />
+                  <button className="button-use">ใช้</button>
+                </div>
+                <p className="text-price-order between mt-32">
+                  ค่าสินค้า <span>990 บาท </span>
+                </p>
+                <p className="text-price-order between">
+                  ค่าจัดส่ง <span>ฟรี </span>
+                </p>
+                <hr className="line-order-bottom" />
+                <p className="text-price-order between text-pink">
+                  ยอดที่ต้องชำระ<span>1,030 บาท</span>
+                </p>
+                <button className="btn-buy-payment">ดำเนินการชำระเงิน</button>
+              </div>
 
+              <p className="text-price-order mt-32">รับชำระด้วย</p>
+              <img src={Logo_payment_1} className="logo-payment-1" />
+              <img src={Logo_payment_2} className="logo-payment-1" />
+              <img src={Logo_payment_3} className="logo-payment-1" />
+              <img src={Logo_payment_4} className="logo-payment-4" />
+              <img src={Logo_payment_5} className="logo-payment-5" />
+            </div>
+          </div>
+        </div>
+        {/*         //Logo_payment_1 */}
         <Footer />
       </div>
     </>
