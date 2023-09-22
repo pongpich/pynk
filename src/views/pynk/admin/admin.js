7d32cda9079d5e15e6625914ef78c27643da36da
Akkewach Yodsomboon <>
2023-09-22 13:02:07 +0700

    เพิ่มปุ่มนิดหน่อย

--- src/views/pynk/admin/admin.js
+++ src/views/pynk/admin/admin.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
 
 function Admin() {
+  const history = useHistory();
   return (
     <div>
       <h1>.</h1>
       <h1>.</h1>
+      <button onClick={() => history.push("/products_management")}>Go to /products_management</button>
       <h1>Admin Page</h1>
       <h1>.</h1>
       <h2>----Login----</h2>
       <h3>Email: </h3>
       <h3>Password: </h3>
+      <button>Login</button>
     </div>
   );
 }
