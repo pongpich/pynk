8f45897949c12631577c381677694e3d7edaa91e
thanet@planforfit.com <lollipopwar-love@hotmail.co.th>
2023-09-26 16:12:01 +0700

    หน้าหมวดหมู่

--- src/views/pynk/admin/shop_category.js
+++ src/views/pynk/admin/shop_category.js
@@ -4,6 +4,7 @@ import colors from "../colors";
 import Footer from "../footer";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import ReactPaginate from "react-paginate";
 
 import icon_cart_white from "../../../assets/img/pynk/shop/icon_cart_white.png";
 import chevron_left_default from "../../../assets/img/pynk/shop/chevron-left-default.png";
@@ -20,7 +21,7 @@ const Shop_category = ({ match }) => {
   const [product, setProduct] = useState(null);
   const [productSlice, setProductSlice] = useState(null);
   const [category_name, setCategory_name] = useState(null);
-  const [currentPage, setCurrentPage] = useState(1);
+  const [itemOffset, setItemOffset] = useState(0);
   const itemsPerPage = 16; // จำนวนรายการต่อหน้า
 
   const dataSelect = [
@@ -34,14 +35,6 @@ const Shop_category = ({ match }) => {
     { category: "food_supplement", name: "อาหารเสริม" },
     { category: "another", name: "อื่นๆ" },
   ];
-
-  useEffect(() => {
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    const currentItems = product && product.slice(startIndex, endIndex);
-    setProductSlice(currentItems);
-    console.log("currentItems", endIndex);
-  }, [product, currentPage]);
 
   useEffect(() => {
     setProduct(
@@ -71,18 +64,55 @@ const Shop_category = ({ match }) => {
     );
   };
 
-  const handlePageChange = (pageNumber) => {
-    setCurrentPage(pageNumber);
+  // Simulate fetching items from another resources.
+  // (This could be items from props; or items loaded in a local state
+  // from an API endpoint with useEffect and useState)
+  const endOffset = itemOffset + itemsPerPage;
+  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+  const currentItems = product && product.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(product && product.length / itemsPerPage);
+  const maxItemOffset = (pageCount - 1) * itemsPerPage;
+  // Invoke when user click to request another page.
+
+  useEffect(() => {
+    setProductSlice(currentItems);
+  }, [product, itemOffset]);
+  const handlePageClick = (event) => {
+    const newOffset = event.selected * itemsPerPage;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
   };
 
-  const pageNumbers = [];
-  for (
-    let i = 1;
-    i <= Math.ceil(product && product.length / itemsPerPage);
-    i++
-  ) {
-    pageNumbers.push(i);
-  }
+  const pagination_category = () => {
+    return (
+      <ReactPaginate
+        previousLabel={
+          <img
+            src={itemOffset === 0 ? chevron_left_default : chevron_left}
+            alt="Previous"
+            style={{ width: "24px", height: "24px" }}
+          />
+        }
+        nextLabel={
+          <img
+            src={
+              itemOffset === maxItemOffset
+                ? chevron_right_default
+                : chevron_right
+            }
+            alt="Previous"
+            style={{ width: "24px", height: "24px" }}
+          />
+        }
+        pageCount={pageCount}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination"} // Replace with your CSS class name
+        activeClassName={"active-pagination"} // Replace with your CSS class name
+      />
+    );
+  };
 
   return (
     <div className="shop-category">
@@ -97,13 +127,13 @@ const Shop_category = ({ match }) => {
           </p>
           <div className="col-12 col-md-3">
             <select
-              class="form-select text-category-select"
+              className="form-select text-category-select"
               aria-label="Default select example"
               onChange={selectCategory}
             >
-              <option selected disabled>
+              {/* <option selected disabled>
                 Open this select menu
-              </option>
+              </option> */}
               {dataSelect &&
                 dataSelect.map((item, index) => (
                   <option
@@ -117,107 +147,56 @@ const Shop_category = ({ match }) => {
             </select>
           </div>
         </div>
-        {productSlice &&
-          productSlice.map((item, index) => (
-            <Link to={`/shop_details/${item.product_id}`}>
-              <div className="product-category" key={index}>
-                <div className="box-item-hover cursor-pointer">
-                  <p className="hot-shop-details">HOT</p>
-                  <img src={item.image_url} className="image-slider" />
-                  <div className="slider-hr" />
-                  <p className="text-center text-head-slider">
-                    {item.product_name}
-                  </p>
-                  <p className="text-center text-slider-hover">
-                    ฿{item.price.toLocaleString()}{" "}
-                    <span className="slide-span">฿199 </span>
-                  </p>
+        <div className="product-center">
+          {productSlice &&
+            productSlice.map((item, index) => (
+              <div className="product-left">
+                <Link to={`/shop_details/${item.product_id}`}>
+                  <div className="product-category " key={index}>
+                    <div className="box-item-hover cursor-pointer">
+                      <p className="hot-shop-details">HOT</p>
+                      <img src={item.image_url} className="image-slider" />
+                      <div className="slider-hr" />
+                      <p className="text-center text-head-slider">
+                        {item.product_name}
+                      </p>
+                      <p className="text-center text-slider-hover">
+                        ฿{item.price.toLocaleString()}{" "}
+                        <span className="slide-span">฿199 </span>
+                      </p>
 
-                  <button
-                    type="button"
-                    className="btn  add-shopping-bag "
-                    style={{
-                      backgroundColor: colors.primary4,
-                      width: "100%",
-                      borderRadius: 46,
-                      marginTop: 0,
-                      marginBottom: 32,
-                      border: 0,
-                      position: "relative",
-                    }}
-                  >
-                    <span className="add_shop-test_span ">
-                      <img
-                        width={18}
-                        height={18}
-                        src={icon_cart_white}
-                        className="span-img-category"
-                      />
-                      เพิ่มลงถุงช้อปปิ้ง
-                    </span>
-                  </button>
-                </div>
-              </div>
-            </Link>
-          ))}
-        {pageNumbers && pageNumbers.length > 1 && (
-          <div className="box-pagination">
-            <div className="pagination">
-              <img
-                src={currentPage == 1 ? chevron_left_default : chevron_left}
-                className={
-                  currentPage != 1
-                    ? "chevron-left cursor-pointer"
-                    : "chevron-left"
-                }
-                onClick={() =>
-                  handlePageChange(
-                    currentPage == 1 ? currentPage : currentPage - 1
-                  )
-                }
-              />
-              {pageNumbers.map((number, index) => (
-                <React.Fragment key={index}>
-                  <button
-                    onClick={() => handlePageChange(number)}
-                    className={
-                      currentPage === number ? "active-pagination" : ""
-                    }
-                  >
-                    {number}
-                  </button>
-                  {index === pageNumbers.length - 2 &&
-                    currentPage < pageNumbers.length - 1 && (
-                      <div
-                        className="page-item disabled"
-                        key={`ellipsis-${index}`}
+                      <button
+                        type="button"
+                        className="btn  add-shopping-bag"
+                        style={{
+                          backgroundColor: colors.primary4,
+                          width: "100%",
+                          borderRadius: 46,
+                          marginTop: 0,
+
+                          border: 0,
+                          position: "relative",
+                        }}
                       >
-                        <span className="page-item-span">...</span>
-                      </div>
-                    )}
-                </React.Fragment>
-              ))}
-              <img
-                src={
-                  currentPage == pageNumbers.length
-                    ? chevron_right_default
-                    : chevron_right
-                }
-                className={
-                  currentPage != pageNumbers.length
-                    ? "chevron-right cursor-pointer"
-                    : "chevron-right"
-                }
-                onClick={() =>
-                  handlePageChange(
-                    currentPage == pageNumbers.length
-                      ? currentPage
-                      : currentPage + 1
-                  )
-                }
-              />
-            </div>
-          </div>
+                        <span className="add_shop-test_span ">
+                          <img
+                            width={18}
+                            height={18}
+                            src={icon_cart_white}
+                            className="span-img-category"
+                          />
+                          เพิ่มลงถุงช้อปปิ้ง
+                        </span>
+                      </button>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            ))}
+        </div>
+
+        {pageCount > 1 && (
+          <div className="box-pagination">{pagination_category()}</div>
         )}
       </div>
 
