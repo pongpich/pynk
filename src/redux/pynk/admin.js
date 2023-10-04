a01e7d3b01c764a314836b801e1d75ca832b164f
Akkewach Yodsomboon <>
2023-10-05 05:22:54 +0700

    จบงานหน้าเพิ่มสินค้า

--- src/redux/pynk/admin.js
+++ src/redux/pynk/admin.js
@@ -2,97 +2,220 @@ import { all, call, fork, put, takeEvery } from "redux-saga/effects";
 import { API } from "aws-amplify";
 
 /* ACTION Section */
-
 export const types = {
-  GET_PRODUCT_DETAIL: "GET_PRODUCT_DETAIL",
-  GET_PRODUCT_DETAIL_SUCCESS: "GET_PRODUCT_DETAIL_SUCCESS",
-  GET_PRODUCT_DETAIL_FAIL: "GET_PRODUCT_DETAIL_FAIL",
+    GET_PRODUCT_DETAIL: "GET_PRODUCT_DETAIL",
+    GET_PRODUCT_DETAIL_SUCCESS: "GET_PRODUCT_DETAIL_SUCCESS",
+    GET_PRODUCT_DETAIL_FAIL: "GET_PRODUCT_DETAIL_FAIL",
+    ADD_PRODUCT: "ADD_PRODUCT",
+    ADD_PRODUCT_SUCCESS: "ADD_PRODUCT_SUCCESS",
+    ADD_PRODUCT_FAIL: "ADD_PRODUCT_FAIL",
 };
 
+/* ADD_PRODUCT */
+export const add_product = (
+    product_id,
+    product_name,
+    category,
+    price,
+    available_stock,
+    image_list,
+    description,
+    nutritional_value,
+    detail
+) => ({
+    type: types.ADD_PRODUCT,
+    payload: {
+        product_id,
+        product_name,
+        category,
+        price,
+        available_stock,
+        image_list,
+        description,
+        nutritional_value,
+        detail
+    }
+});
 
+const addProductSagaAsync = async (
+    product_id,
+    product_name,
+    category,
+    price,
+    available_stock,
+    image_list,
+    description,
+    nutritional_value,
+    detail
+) => {
+    try {
+        const apiResult = await API.post("pynk", "/add_product", {
+            body: {
+                product_id,
+                product_name,
+                category,
+                price,
+                available_stock,
+                image_list,
+                description,
+                nutritional_value,
+                detail
+            },
+        });
+        return apiResult;
+    } catch (error) {
+        return { error, messsage: error.message };
+    }
+};
+
+function* addProductSaga({ payload }) {
+    const {
+        product_id,
+        product_name,
+        category,
+        price,
+        available_stock,
+        image_list,
+        description,
+        nutritional_value,
+        detail
+    } = payload;
+
+    try {
+        const apiResult = yield call(
+            addProductSagaAsync,
+            product_id,
+            product_name,
+            category,
+            price,
+            available_stock,
+            image_list,
+            description,
+            nutritional_value,
+            detail
+        );
+
+        if (apiResult.results.message === "success") {
+            yield put({
+                type: types.ADD_PRODUCT_SUCCESS
+            })
+        }
+    } catch (error) {
+        
+        yield put({
+            type: types.ADD_PRODUCT_FAIL
+        })
+        console.log("error from addProductSaga :", error);
+    }
+};
+
+export function* watchAddProductSaga() {
+    yield takeEvery(types.ADD_PRODUCT, addProductSaga);
+};
+
+/* GET_PRODUCT_DETAIL  */
 export const getProductDetail = (sku) => ({
-  type: types.GET_PRODUCT_DETAIL,
-  payload: {
-    sku
-  }
+    type: types.GET_PRODUCT_DETAIL,
+    payload: {
+        sku
+    }
 });
 
 const getProductDetailSagaAsync = async (
-  sku
+    sku
 ) => {
-  try {
-    const apiResult = await API.get("pynk", "/getProductDetail", {
-      queryStringParameters: {
-        sku
-      },
-    });
-    return apiResult;
-  } catch (error) {
-    return { error, messsage: error.message };
-  }
+    try {
+        const apiResult = await API.get("pynk", "/getProductDetail", {
+            queryStringParameters: {
+                sku
+            },
+        });
+        return apiResult;
+    } catch (error) {
+        return { error, messsage: error.message };
+    }
 };
 
 function* getProductDetailSaga({ payload }) {
-  const {
-    sku
-  } = payload
+    const {
+        sku
+    } = payload
 
-  try {
-    const apiResult = yield call(
-      getProductDetailSagaAsync,
-      sku
-    );
+    try {
+        const apiResult = yield call(
+            getProductDetailSagaAsync,
+            sku
+        );
 
-    if (apiResult.results.message === "success") {
-      yield put({
-        type: types.GET_PRODUCT_DETAIL_SUCCESS,
-        payload: apiResult.results.product
-      })
-    } else if (apiResult.results.message === "fail") {
-      yield put({
-        type: types.GET_PRODUCT_DETAIL_FAIL
-      })
+        if (apiResult.results.message === "success") {
+            yield put({
+                type: types.GET_PRODUCT_DETAIL_SUCCESS,
+                payload: apiResult.results.product
+            })
+        } else if (apiResult.results.message === "fail") {
+            yield put({
+                type: types.GET_PRODUCT_DETAIL_FAIL
+            })
+        }
+
+    } catch (error) {
+        console.log("error from getProductDetailSaga :", error);
     }
-
-  } catch (error) {
-    console.log("error from getProductDetailSaga :", error);
-  }
-}
+};
 
 export function* watchGetProductDetailSaga() {
-  yield takeEvery(types.GET_PRODUCT_DETAIL, getProductDetailSaga);
-}
+    yield takeEvery(types.GET_PRODUCT_DETAIL, getProductDetailSaga);
+};
 
+/* FORK  Section */
 export function* saga() {
-  yield all([
-    fork(watchGetProductDetailSaga),
-  ]);
+    yield all([
+        fork(watchGetProductDetailSaga),
+        fork(watchAddProductSaga),
+    ]);
 }
 
+/* REDUCER  Section */
 const INIT_STATE = {
-  product_detail_zort: null,
-  status_get_product_detail_zort: "default"
+    product_detail_zort: null,
+    status_get_product_detail_zort: "default",
+    status_add_product: "default",
 };
 
 export function reducer(state = INIT_STATE, action) {
-  switch (action.type) {
-    case types.GET_PRODUCT_DETAIL:
-      return {
-        ...state,
-        status_get_product_detail_zort: "loading",
-      };
-    case types.GET_PRODUCT_DETAIL_SUCCESS:
-      return {
-        ...state,
-        product_detail_zort: action.payload,
-        status_get_product_detail_zort: "success",
-      };
-    case types.GET_PRODUCT_DETAIL_FAIL:
-      return {
-        ...state,
-        status_get_product_detail_zort: "fail",
-      };
-    default:
-      return { ...state };
-  }
+    switch (action.type) {
+        case types.ADD_PRODUCT:
+            return {
+                ...state,
+                status_add_product: "loading",
+            };
+        case types.ADD_PRODUCT_SUCCESS:
+            return {
+                ...state,
+                status_add_product: "success",
+            };
+        case types.ADD_PRODUCT_FAIL:
+            return {
+                ...state,
+                status_add_product: "fail",
+            };
+        case types.GET_PRODUCT_DETAIL:
+            return {
+                ...state,
+                status_get_product_detail_zort: "loading",
+            };
+        case types.GET_PRODUCT_DETAIL_SUCCESS:
+            return {
+                ...state,
+                product_detail_zort: action.payload,
+                status_get_product_detail_zort: "success",
+            };
+        case types.GET_PRODUCT_DETAIL_FAIL:
+            return {
+                ...state,
+                status_get_product_detail_zort: "fail",
+            };
+        default:
+            return { ...state };
+    }
 }
--- src/views/pynk/admin/products_management.js
+++ src/views/pynk/admin/products_management.js
@@ -1,61 +1,353 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { getProductDetail } from "../../../redux/pynk/admin"
+import { getProductDetail, add_product } from "../../../redux/pynk/admin"
 
 function ProductsManagement() {
+    const history = useHistory();
     const dispatch = useDispatch();
-    const status_get_product_detail_zort = useSelector(({ admin }) => (admin ? admin.status_get_product_detail_zort : ""));
+    const user = useSelector(({ auth }) => (auth ? auth.user : ""));
     const product_detail_zort = useSelector(({ admin }) => (admin ? admin.product_detail_zort : ""));
+    const status_get_product_detail_zort = useSelector(({ admin }) => (admin ? admin.status_get_product_detail_zort : ""));
+    const status_add_product = useSelector(({ admin }) => (admin ? admin.status_add_product : ""));
 
-
-    // สร้าง state สำหรับเก็บค่าของ input
     const [inputValue, setInputValue] = useState('');
+    const [description, setDescription] = useState('');
+    const [detail, setDetail] = useState('');
+    const [selectedCategory, setSelectedCategory] = useState('another'); // เริ่มต้นค่าเป็นช่องว่าง
+    const [isNutritionalInfoProvided, setIsNutritionalInfoProvided] = useState(false);
+    const [showProductDetail, setShowProductDetail] = useState(false);
+    const [msgAddProductSuccess, setMsgAddProductSuccess] = useState(false);
+    const [msgAddProductFail, setMsgAddProductFail] = useState(false);
+    const [msgGetProductFail, setMsgGetProductFail] = useState(false);
 
-    // ฟังก์ชั่น handleInputChange ใช้ในการอัปเดตค่า state เมื่อมีการเปลี่ยนแปลงใน input
+    const [nutritionalInfoList, setNutritionalInfoList] = useState([
+        { nutrition_name: '', value: '', unit: '' },
+    ]);
+
+    const handleNutritionalInfoChange = (event, index, field) => {
+        const updatedNutritionalInfoList = [...nutritionalInfoList];
+        updatedNutritionalInfoList[index][field] = event.target.value;
+        console.log("nutritionalInfoList :", nutritionalInfoList);
+        setNutritionalInfoList(updatedNutritionalInfoList);
+    };
+
+    const handleAddRow = () => {
+        setNutritionalInfoList([...nutritionalInfoList, { nutrition_name: '', value: '', unit: '' }]);
+    };
+
+    const handleRemoveRow = (index) => {
+        const updatedNutritionalInfoList = [...nutritionalInfoList];
+        updatedNutritionalInfoList.splice(index, 1);
+        setNutritionalInfoList(updatedNutritionalInfoList);
+    };
+
+    const handleRadioChange = (event) => {
+        setIsNutritionalInfoProvided(event.target.value === 'yes');
+        // เมื่อผู้ใช้เลือก "ใช่" ให้เปิดใช้งานช่องกรอกข้อมูลสารอาหาร
+        // เมื่อผู้ใช้เลือก "ไม่" ให้ปิดใช้งานช่องกรอกข้อมูลสารอาหาร
+    };
+
     const handleInputChange = (event) => {
         setInputValue(event.target.value);
     };
 
-    const handleSubmit = () => {
-        // ในฟังก์ชั่นนี้คุณสามารถดำเนินการกับค่า inputValue หรือส่งข้อมูลไปที่เซิร์ฟเวอร์ตามที่คุณต้องการ
-        console.log('ค่าที่คุณกรอกคือ:', inputValue);
-        dispatch(getProductDetail(inputValue))
-
+    const handleCategoryChange = (event) => {
+        setSelectedCategory(event.target.value);
     };
 
-    return (
-        <div>
-            <h1 style={{ color: "white" }}>.</h1>
-            <h1 style={{ color: "white" }}>.</h1>
+    const handleDescriptionChange = (event) => {
+        setDescription(event.target.value);
+    };
 
-            <h1>เพิ่มรายการสินค้า</h1>
-            <div className='d-flex' style={{ alignItems: "center" }}>
+    const handleDetailChange = (event) => {
+        setDetail(event.target.value);
+    };
 
-                <h3>รหัสสินค้า:</h3>
-                <input
-                    style={{ width: "20%" }}
-                    type="text"
-                    value={inputValue} // ใช้ค่า state เป็นค่า value ของ input
-                    onChange={handleInputChange} // เมื่อมีการเปลี่ยนแปลงใน input จะเรียกฟังก์ชั่น handleInputChange
-                />
-                <button style={{ height: 55, width: 100 }} onClick={handleSubmit}>ค้นหา</button>
+    const handleSearch = () => {
+        //สั่งเคลียค่า status ต่างๆ ตอนจังหวะกำลังค้นหาข้อมูล
+        setMsgAddProductSuccess(false);
+        setMsgAddProductFail(false);
+        setMsgGetProductFail(false);
+
+        //เรียกใช้ API
+        dispatch(getProductDetail(inputValue));
+    };
+
+    useEffect(() => {
+        //ถ้าดึงข้อมูลสินค้าจาก Zort สำเร็จให้โชว์รายละเอียดสินค้า
+        if (status_get_product_detail_zort === "success") {
+            setShowProductDetail(true);
+        }
+        //ถ้าดึงข้อมูลสินค้าจาก Zort ไม่สำเร็จให้โชว์ข้อความ Error
+        if (status_get_product_detail_zort === "fail") {
+            setMsgGetProductFail(true);
+            setShowProductDetail(false);
+        }
+    }, [status_get_product_detail_zort]);
+
+    useEffect(() => {
+        if (status_add_product === "success") {
+            setShowProductDetail(false);
+            setMsgAddProductSuccess(true);
+        }
+        if (status_add_product === "fail") {
+            setShowProductDetail(false);
+            setMsgAddProductFail(true);
+        }
+    }, [status_add_product]);
+
+    useEffect(() => {
+        //เช็คว่าถ้าไม่ใช่ admin ให้เตะออกจากหน้านี้
+        if (!user || (user && user.authorization !== "admin")) {
+            history.push("/home");
+        }
+
+        //สั่งคืนค่าป้องกันค่าค้าง
+        setShowProductDetail(false);
+        setMsgAddProductSuccess(false);
+        setMsgAddProductFail(false);
+    }, []);
+
+    function onAddProduct(
+        product_id,
+        product_name,
+        category,
+        price,
+        available_stock,
+        image_list,
+        description,
+        nutritional_value,
+        detail
+    ) {
+        dispatch(add_product(
+            product_id,
+            product_name,
+            category,
+            price,
+            available_stock,
+            image_list,
+            description,
+            nutritional_value,
+            detail
+        ));
+    };
+
+    const renderAddProduct = () => {
+        return (
+            <div>
+                <h1 style={{ color: "white" }}>.</h1>
+                <h1 style={{ color: "white" }}>.</h1>
+
+                <div className='pointer bold text-primary'>{`< สินค้า`}</div>
+                <h2 className='mb-5'>เพิ่มสินค้าใหม่</h2>
+
+                <div className='text-danger'>** กรอกรหัสสินค้าจากเว็บไซต์ Zort **</div>
+                <div className='d-flex gap-2' style={{ alignItems: "center" }}>
+                    <h5 className='bold'>รหัสสินค้า:</h5>
+                    <input
+                        style={{ width: "20%", height: 40 }}
+                        type="text"
+                        value={inputValue} // ใช้ค่า state เป็นค่า value ของ input
+                        onChange={handleInputChange} // เมื่อมีการเปลี่ยนแปลงใน input จะเรียกฟังก์ชั่น handleInputChange
+                    />
+                    <button className='buy-now' style={{ height: 50, width: 100, borderRadius: 10 }} onClick={handleSearch}>ค้นหา</button>
+                </div>
+                {
+                    msgGetProductFail &&
+                    <div>
+                        <h1 className='text-danger'><i class="fa-solid fa-xmark"></i> ไม่พบสินค้า</h1>
+                        <div className='text-danger'>โปรดตัวสอบรหัสสินค้าให้ถูกต้อง</div>
+                    </div>
+                }
+
+                {
+                    showProductDetail ?
+                        <div className='card' style={{ padding: 20 }}>
+                            <div className='d-flex mt-5 gap-3'>
+                                {
+                                    (product_detail_zort && product_detail_zort.imageList) ?
+                                        product_detail_zort.imageList.map((imageUrl, index) => (
+                                            <img key={index} src={imageUrl} alt={`Image ${index}`} width={200} style={{ margin: 2 }} />
+                                        ))
+                                        :
+                                        <></>
+                                }
+                            </div>
+                            <div className='mt-5'><span className='bold'>ชื่อสินค้า:</span> {product_detail_zort && product_detail_zort.name}</div>
+                            <div><span className='bold'>ราคา:</span> {product_detail_zort && product_detail_zort.sellprice} บาท</div>
+                            <div><span className='bold'>จำนวนคงเหลือ:</span> {product_detail_zort && product_detail_zort.availablestock}</div>
+
+                            <div className='mt-3' style={{ display: 'inline-block', marginRight: '10px' }}><span className='bold'>หมวดหมู่สินค้า:</span></div>
+                            <select id="category" value={selectedCategory} onChange={handleCategoryChange} style={{ width: 300 }}>
+                                <option value="exercise_equipment">อุปกรณ์ออกกำลังกาย</option>
+                                <option value="fitto_plant_protein">Fitto Plant Protein</option>
+                                <option value="fitto_pre_workout_fat_burner">Fitto Pre-Work Out & Fat Burner</option>
+                                <option value="fitto_drink">Fitto Drink</option>
+                                <option value="food_supplement">อาหารเสริม</option>
+                                <option value="another">อื่นๆ</option>
+                                {/* เพิ่ม options ของ category เพิ่มเติมตามที่คุณต้องการ */}
+                            </select>
+                            {/*  <p>คุณเลือก: {selectedCategory}</p> */}
+
+                            <div className='mt-3'><span className='bold'>คำอธิบายสินค้า:</span></div>
+                            <textarea
+                                id="description"
+                                value={description}
+                                onChange={handleDescriptionChange}
+                                rows={4} // กำหนดจำนวนแถวที่แสดง
+                                cols={50} // กำหนดความกว้างของ textarea
+                            />
+                            {/* <p>คำอธิบาย: {description}</p> */}
+
+                            <div className='mt-3'><span className='bold'>รายละเอียดสินค้า:</span></div>
+                            <textarea
+                                id="detail"
+                                value={detail}
+                                onChange={handleDetailChange}
+                                rows={4} // กำหนดจำนวนแถวที่แสดง
+                                cols={50} // กำหนดความกว้างของ textarea
+                            />
+                            {/* <p>รายละเอียดสินค้า: {detail}</p> */}
+
+                            <div className='mt-3'><span className='bold'>สารอาหาร:</span></div>
+                            <div>
+                                <div>
+                                    <label>คุณต้องการกรอกข้อมูลสารอาหารหรือไม่?</label>
+
+                                    <div className='d-flex align-items-center gap-2'>
+                                        <input
+                                            type="radio"
+                                            id="no"
+                                            name="nutritionalInfo"
+                                            value="no"
+                                            checked={!isNutritionalInfoProvided}
+                                            onChange={handleRadioChange}
+                                        />
+                                        <div>ไม่</div>
+                                    </div>
+                                    <div className='d-flex  align-items-center gap-2'>
+                                        <input
+                                            type="radio"
+                                            id="yes"
+                                            name="nutritionalInfo"
+                                            value="yes"
+                                            checked={isNutritionalInfoProvided}
+                                            onChange={handleRadioChange}
+                                        />
+                                        <div>ใช่</div>
+                                    </div>
+
+                                </div>
+                                {isNutritionalInfoProvided && (
+                                    <div>
+                                        <label>ข้อมูลสารอาหาร:</label>
+                                        <div className='text-danger bold'>ตัวอย่างการกรอก</div>
+
+                                        <thead >
+                                            <tr>
+                                                <th scope="col"></th>
+                                                <th scope="col">ชื่อสารอาหาร</th>
+                                                <th scope="col">ปริมาณ</th>
+                                                <th scope="col">หน่วย</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <th scope="row">1. </th>
+                                                <td>พลังงาน</td>
+                                                <td>120</td>
+                                                <td>แคลอรี่</td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row">2. </th>
+                                                <td>โปรตีน</td>
+                                                <td>20</td>
+                                                <td>กรัม</td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row">3. </th>
+                                                <td>น้ำตาล</td>
+                                                <td>2</td>
+                                                <td>กรัม</td>
+                                            </tr>
+                                        </tbody>
+
+                                        {nutritionalInfoList.map((info, index) => (
+                                            <div key={index}>
+                                                {`${index + 1}. `}
+                                                <input
+                                                    type="text"
+                                                    placeholder="ชื่อสารอาหาร"
+                                                    value={info.nutrition_name}
+                                                    onChange={(event) => handleNutritionalInfoChange(event, index, 'nutrition_name')}
+                                                />
+                                                <input
+                                                    type="text"
+                                                    placeholder="ปริมาณ"
+                                                    value={info.value}
+                                                    onChange={(event) => handleNutritionalInfoChange(event, index, 'value')}
+                                                />
+                                                <input
+                                                    type="text"
+                                                    placeholder="หน่วย"
+                                                    value={info.unit}
+                                                    onChange={(event) => handleNutritionalInfoChange(event, index, 'unit')}
+                                                />
+                                                <button className='buy-now' style={{ width: 80 }} onClick={() => handleRemoveRow(index)}>ลบ</button>
+                                            </div>
+                                        ))}
+                                        <button className='buy-now' style={{ width: 150 }} onClick={handleAddRow}>+ เพิ่มชุดใหม่</button>
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+                        :
+                        <div className='card' style={{ padding: 100 }}>
+                        </div>
+                }
+
+                <center style={{ display: showProductDetail ? "block" : "none" }}>
+                    {
+                        (status_add_product !== "loading") &&
+                        <button
+                            className='buy-now'
+                            style={{ height: 55, width: 100, borderRadius: 10 }}
+                            onClick={() => onAddProduct(
+                                product_detail_zort.sku, //product_id,
+                                product_detail_zort.name, //product_name,
+                                selectedCategory, //category,
+                                product_detail_zort.sellprice, //price,
+                                product_detail_zort.availablestock, //available_stock,
+                                product_detail_zort.imageList, //image_list,
+                                description, //description,
+                                nutritionalInfoList, //nutritional_value,
+                                detail, //detail
+                            )}
+                        >
+                            ยืนยัน
+                        </button>
+                    }
+                </center>
+
+                {
+                    msgAddProductSuccess &&
+                    <h1 className='text-success'><i class="fa-solid fa-check"></i> เพิ่มสินค้าสำเร็จ</h1>
+                }
+                {
+                    msgAddProductFail &&
+                    <div>
+                        <h1 className='text-danger'><i class="fa-solid fa-xmark"></i> เพิ่มสินค้าไม่สำเร็จ</h1>
+                        <div className='text-danger'>หากมีรหัสสินค้านี้อยู่แล้วในระบบ ไม่สามารถเพิ่มสินค้าซ้ำได้</div>
+                    </div>
+                }
             </div>
+        );
+    }
 
-            {
-                (product_detail_zort && product_detail_zort.imageList) ?
-                    product_detail_zort.imageList.map((imageUrl, index) => (
-                        <img key={index} src={imageUrl} alt={`Image ${index}`} width={400} style={{ margin: 2 }} />
-                    ))
-                    :
-                    <></>
-            }
-            <div>name: {product_detail_zort && product_detail_zort.name}</div>
-            <div>price: {product_detail_zort && product_detail_zort.sellprice}</div>
-            <div>description: ไม่มีจาก Zort</div>
-            <div>สารอาหาร: ไม่มีจาก Zort</div>
-            <div>stock: {product_detail_zort &&product_detail_zort.availablestock}</div>
-            <div>รายละเอียดสินค้า:ไม่มีจาก Zort</div>
-            <button style={{ height: 55, width: 100 }} onClick={handleSubmit}>เพิ่มสินค้า</button>
+    return (
+        <div style={{ padding: 20 }}>
+            {renderAddProduct()}
         </div>
     );
 }
