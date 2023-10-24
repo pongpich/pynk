75fefbb79eb9dafd03d85426dfedbf6b8db1dc68
Akkewach Yodsomboon <>
2023-10-24 18:23:36 +0700

    ตอน login สำเร็จ ส่งไปหน้า profile

--- src/views/pynk/login.js
+++ src/views/pynk/login.js
@@ -218,7 +218,7 @@ const Login = () => {
 
   useEffect(() => {
     if (statusLogin === "success") {
-      history.push("/shop");
+      history.push("/profile-pynk");
     }
     if (statusLogin === "fail") {
       setIsLoginError("invalidLogin");
