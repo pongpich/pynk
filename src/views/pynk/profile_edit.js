047e982a330ed16317fb7151eca00a620ba767b1
thanet@planforfit.com <lollipopwar-love@hotmail.co.th>
2023-10-25 15:04:51 +0700

    form ชื่อ

--- src/views/pynk/profile_edit.js
+++ src/views/pynk/profile_edit.js
@@ -4,6 +4,9 @@ import { useSelector, useDispatch } from "react-redux";
 import icon_profile from "../../assets/img/pynk/shop/profile.png";
 import { useHistory } from "react-router-dom";
 import rightContent from "../../assets/img/pynk/shop/RightContent.png";
+import icon_facebook from "../../assets/img/pynk/shop/icon-facebook.png";
+import icon_Google from "../../assets/img/pynk/shop/icon-Google.png";
+import icon_line from "../../assets/img/pynk/shop/icon-line.png";
 
 import "./css/profile.css";
 
@@ -13,61 +16,52 @@ const Profile_edit = () => {
   const [statusProfile, setStatusProfile] = useState(1);
 
   const personalInformation = () => {
+    console.log("user", user);
     return (
-      <>
+      <div>
         <p className="text-modal-body">ข้อมูลส่วนตัว</p>
         <div className="row row-16">
           <div class="col-12 col-sm-12 col-md-6 mb-3">
             <label for="exampleFormControlInput1" class="form-label">
-              Email address
+              ชื่อ <span>*</span>
             </label>
             <input
-              type="email"
+              type="text"
               class="form-control"
               id="exampleFormControlInput1"
-              placeholder="name@example.com"
+              placeholder="กรุณาระบุชื่อ"
             />
           </div>
           <div class="col-12 col-sm-12  col-md-6  mb-3">
             <label for="exampleFormControlInput1" class="form-label">
-              Email address
+              นามสกุล <span>*</span>
             </label>
             <input
-              type="email"
+              type="text"
               class="form-control"
               id="exampleFormControlInput1"
-              placeholder="name@example.com"
+              placeholder="กรุณาระบุนามสกุล"
             />
           </div>
         </div>
         <div className="row row-16">
           <div class="col-12 col-sm-12 col-md-6 mb-3">
             <label for="exampleFormControlInput1" class="form-label">
-              Email address
+              เบอร์โทรศัพท์ที่ติดต่อได้ <span>*</span>
             </label>
             <input
-              type="email"
+              type="number"
               class="form-control"
               id="exampleFormControlInput1"
-              placeholder="name@example.com"
-            />
-          </div>
-          <div class="col-12 col-sm-12  col-md-6  mb-3">
-            <label for="exampleFormControlInput1" class="form-label">
-              Email address
-            </label>
-            <input
-              type="email"
-              class="form-control"
-              id="exampleFormControlInput1"
-              placeholder="name@example.com"
+              placeholder="0XX-XXX-XXXX"
             />
           </div>
         </div>
+        <p className="text-modal-body mt-64">บัญชีเข้าใช้งาน</p>
         <div className="row row-16">
           <div class="col-12 col-sm-12 col-md-6 mb-3">
             <label for="exampleFormControlInput1" class="form-label">
-              Email address
+              อีเมลเข้าใช้งาน <span>*</span>
             </label>
             <input
               type="email"
@@ -78,7 +72,7 @@ const Profile_edit = () => {
           </div>
           <div class="col-12 col-sm-12  col-md-6  mb-3">
             <label for="exampleFormControlInput1" class="form-label">
-              Email address
+              รหัสผ่าน <span>*</span>
             </label>
             <input
               type="email"
@@ -114,61 +108,44 @@ const Profile_edit = () => {
         </div>
 
         <div className="row row-16">
-          <div class="col-12 col-sm-12 col-md-6 mb-3">
+          <div class="col-12 col-sm-12 col-md-12 mb-3">
             <label for="exampleFormControlInput1" class="form-label">
-              Email address
+              การเชื่อมต่อ
             </label>
-            <input
-              type="email"
-              class="form-control"
-              id="exampleFormControlInput1"
-              placeholder="name@example.com"
-            />
-          </div>
-          <div class="col-12 col-sm-12  col-md-6  mb-3">
-            <label for="exampleFormControlInput1" class="form-label">
-              Email address
-            </label>
-            <input
-              type="email"
-              class="form-control"
-              id="exampleFormControlInput1"
-              placeholder="name@example.com"
-            />
-          </div>
-        </div>
-        <div className="row row-16">
-          <div class="col-12 col-sm-12 col-md-6 mb-3">
-            <label for="exampleFormControlInput1" class="form-label">
-              Email address
-            </label>
-            <input
-              type="email"
-              class="form-control"
-              id="exampleFormControlInput1"
-              placeholder="name@example.com"
-            />
-          </div>
-          <div class="col-12 col-sm-12  col-md-6  mb-3">
-            <label for="exampleFormControlInput1" class="form-label">
-              Email address
-            </label>
-            <input
-              type="email"
-              class="form-control"
-              id="exampleFormControlInput1"
-              placeholder="name@example.com"
-            />
+            <div className="box-fa">
+              <div className="connect-icon">
+                <img src={icon_facebook} className="icon_facebook" />
+                <p className="text-icon-connect">Facebook</p>
+              </div>
+              <button className="btn-connect ">Connect</button>
+            </div>
+            <div className="box-google">
+              <div className="connect-icon">
+                <img src={icon_Google} className="icon_facebook" />
+                <p className="text-icon-connect">Google</p>
+              </div>
+              <button className="btn-connect ">Connect</button>
+            </div>
+            <div className="box-line">
+              <div className="connect-icon">
+                <img src={icon_line} className="icon_facebook" />
+                <p className="text-icon-connect">Line</p>
+              </div>
+              <button className="btn-connect ">Connect</button>
+            </div>
+            <div className="row-16-button">
+              <button className="save-profile">บันทึก</button>
+            </div>
           </div>
         </div>
-      </>
+      </div>
     );
   };
   const addressInformation = () => {
     return (
-      <>
+      <div className="mb-5">
         <p className="text-modal-body">ข้อมูลที่อยู่</p>
-      </>
+      </div>
     );
   };
 
@@ -180,53 +157,54 @@ const Profile_edit = () => {
     <div className="div-edit-profile">
       <div className="head-profile">
         <div className="image-head" />
-
-        <div className="box-edit-profile row row-16">
-          <div className="col-12 col-sm-12 col-md-4 col-lg-4">
-            <div className="box-edit">
-              <div className="icon-box-edit" onClick={onBack}>
-                <img src={icon_profile} className="icon-profile edit-icon" />
-                <p className="bebe-fit text-center">BeBe Fit</p>
-                <p className="username text-center">
-                  @{user && user.first_name}
-                </p>
-              </div>
-              {/*  <p className="lv">LV: มือใหม่หัดฟิตหุ่น</p> */}
-              <div
-                className={
-                  statusProfile == 1
-                    ? "box-right-text  box-right-active"
-                    : "box-right-text"
-                }
-                onClick={() => setStatusProfile(1)}
-              >
-                บัญชีของฉัน{" "}
-                <span>
-                  {" "}
-                  <img src={rightContent} className="icon-rightContent" />
-                </span>
-              </div>
-              <div
-                className={
-                  statusProfile == 2
-                    ? "box-right-text  box-right-active"
-                    : "box-right-text"
-                }
-                onClick={() => setStatusProfile(2)}
-              >
-                ข้อมูลที่อยู่{" "}
-                <span>
-                  {" "}
-                  <img src={rightContent} className="icon-rightContent" />
-                </span>
+        <div className="box-profile2">
+          <div className="box-edit-profile row row-16">
+            <div className="col-12 col-sm-12 col-md-4 col-lg-4">
+              <div className="box-edit">
+                <div className="icon-box-edit" onClick={onBack}>
+                  <img src={icon_profile} className="icon-profile edit-icon" />
+                  <p className="bebe-fit text-center">BeBe Fit</p>
+                  <p className="username text-center">
+                    @{user && user.first_name}
+                  </p>
+                </div>
+                {/*  <p className="lv">LV: มือใหม่หัดฟิตหุ่น</p> */}
+                <div
+                  className={
+                    statusProfile == 1
+                      ? "box-right-text  box-right-active"
+                      : "box-right-text"
+                  }
+                  onClick={() => setStatusProfile(1)}
+                >
+                  บัญชีของฉัน{" "}
+                  <span>
+                    {" "}
+                    <img src={rightContent} className="icon-rightContent" />
+                  </span>
+                </div>
+                <div
+                  className={
+                    statusProfile == 2
+                      ? "box-right-text  box-right-active"
+                      : "box-right-text"
+                  }
+                  onClick={() => setStatusProfile(2)}
+                >
+                  ข้อมูลที่อยู่{" "}
+                  <span>
+                    {" "}
+                    <img src={rightContent} className="icon-rightContent" />
+                  </span>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-12 col-sm-12 col-md-8 col-lg-8">
-            <div className="from-profile">
-              {statusProfile == 1
-                ? personalInformation()
-                : addressInformation()}
+            <div className="col-12 col-sm-12 col-md-8 col-lg-8">
+              <div className="from-profile">
+                {statusProfile == 1
+                  ? personalInformation()
+                  : addressInformation()}
+              </div>
             </div>
           </div>
         </div>
