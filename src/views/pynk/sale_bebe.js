0243ccf0a5fc54463f0e6c8495601266d84d26b5
Sorawit1234 <sorawit@planforfit.com>
2024-03-18 11:05:51 +0700

    update bebe

--- src/views/pynk/sale_bebe.js
+++ src/views/pynk/sale_bebe.js
@@ -291,8 +291,6 @@ export default function SaleBebePage() {
         />
       </Stack>
 
-<<<<<<< HEAD
-<<<<<<< HEAD
       <Box sx={{ background: "#FCDEEC" }}>
         <Container maxWidth="xl">
           <Grid container spacing={3}>
@@ -309,108 +307,6 @@ export default function SaleBebePage() {
                 justifyContent={"center"}
                 height={"100%"}
               >
-=======
-=======
->>>>>>> a7f5a25 (update sale bebe)
-      <Container
-        maxWidth="lg"
-        sx={{
-          backgroundImage: `url(${bg_1})`,
-          backgroundSize: "cover",
-          height: 500,
-          width: "100%",
-          borderRadius: "1.5rem",
-        }}
-      >
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={7}>
-            <Box
-              sx={{
-                background: "#202020",
-                p: 2,
-                height: 100,
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-              mt={7}
-            >
-              <Stack flexDirection={"row"} alignItems={"flex-end"}>
-                <Typography variant="h6" color={"#FFFFFF"}>
-                  ให้การ<span style={{ color: "#FF4ABB" }}>ลดน้ำหนัก</span>
-                  ครั้งนี้
-                </Typography>
-                <Typography variant="h3" color={"#FFFFFF"} sx={{ p: "-20px" }}>
-                  เป็นครั้งสุดท้าย
-                </Typography>
-              </Stack>
-            </Box>
-
-            <Stack
-              flexDirection={"row"}
-              flexWrap={"wrap"}
-              alignItems={"center"}
-              ml={2}
-            >
-              <Box>
-                <img
-                  src={logo}
-                  style={{ width: "40px", height: "80px" }}
-                  alt="logo"
-                />
-              </Box>
-              <Box>
-<<<<<<< HEAD
-<<<<<<< HEAD
-      
-=======
-=======
->>>>>>> a9fcb07 (update ui sale_page (for test))
-                <div style="height:100px; line-height:100px; background:#EEE;">
-                  <span style="vertical-align:middle;">test</span>
-                  <span style="font-size:2em; vertical-align:middle;">
-                    test
-                  </span>
-                </div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-                
->>>>>>> f4e1cd9 (demo push)
-=======
->>>>>>> a9fcb07 (update ui sale_page (for test))
-=======
-                
->>>>>>> f4e1cd9 (demo push)
-
-                <Typography
-                  sx={{ color: "#FCDEEC", fontSize: { xs: 50, lg: 120 } }}
-                  fontWeight={700}
-                >
-                  8 Weeks
-                </Typography>
-<<<<<<< HEAD
->>>>>>> bff9e96 (update ui sale_page (for test))
-=======
-=======
-      <Box sx={{ background: "#FCDEEC" }}>
-        <Container maxWidth="xl">
-          <Grid container spacing={3}>
-            <Grid item xs={12} sm={6} order={{ xs: 2, sm: 1 }}>
-              <img
-                src={babe_2}
-                style={{ width: "100%", height: "auto" }}
-                alt="babe_2"
-              />
-            </Grid>
-            <Grid item xs={12} sm={6} order={{ xs: 1, sm: 2 }}>
-              <Stack
-                flexDirection={"column"}
-                justifyContent={"center"}
-                height={"100%"}
-              >
->>>>>>> c1ea535 (update sale bebe)
->>>>>>> a7f5a25 (update sale bebe)
                 <Typography
                   sx={{
                     typography: { xs: "h3", sm: "h3", md: "h2" },
